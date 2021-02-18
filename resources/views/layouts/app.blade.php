add8bd091c2ee5323e3f93c212188f17b8140723
VanchezMD <ion.boghean@elev.cihcahul.md>
2021-02-18 13:13:33 +0200

    V0.1

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+
+</head>
+<body>
+
+    @yield('content')
+<script src="{{asset('js/jquery.js')}}"></script>
+<script src="{{asset('js/owl.carousel.js')}}"></script>
+<script>
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+</script>
+</body>
+</html>
